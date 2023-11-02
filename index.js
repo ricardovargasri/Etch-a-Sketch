31817bb7351e4c6a8632db608ee008b1ed63243c
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2023-11-02 12:05:24 -0500

    en proceso

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var numerador = document.querySelector('#numerador');
 
 
 numerador.addEventListener('click', ()=>{
-  /* window.location.reload(); */
   cuadros = prompt('cuantos cuadros por cuanto cuadros entre 1 y 16:');
   if(cuadros < 1){
     alert('No puede ser un solo cuadro');
@@ -14,6 +13,8 @@ numerador.addEventListener('click', ()=>{
   else{
     
     cuadros = cuadros**2;
+    container.style.display = 'flex'; // Hacer que el contenedor sea flexible
+    container.style.flexWrap = 'wrap'
     for(i = 0; i < cuadros; i++){
       agregarElemento();
     };
@@ -25,6 +26,9 @@ function agregarElemento(){
   var newDiv = document.createElement('div');
   newDiv.style.width = '12px';
   newDiv.style.height = '12px';
-  newDiv.style.border = '1px solid black';
+  newDiv.style.border = '0.5px solid black';
+  newDiv.style.boxSizing = 'border-box';
+  newDiv.style.display = 'inline-block';
   container.appendChild(newDiv);
 };
+
